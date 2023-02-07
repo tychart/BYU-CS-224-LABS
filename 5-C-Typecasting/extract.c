37e20f3b11aa892d7c57c97fe90b7321bb230333
David Parrott <dmparr22@byu.edu>
2023-02-06 17:10:38 -0700

    Add expected answers for int and short extraction

--- 5-C-Typecasting/extract.c
+++ 5-C-Typecasting/extract.c
@@ -2,20 +2,23 @@
 
 int main()
 {
-    //Initialize 20 bytes of memory
+    // Initialize 20 bytes of memory
     char c[] = {0xf8, 0x24, 0x37, 0x20, 0xd1, 0xa4, 0x13, 0xe0, 0x63, 0x5d, 0x0d, 0x08, 0x64, 0xfd, 0xc9, 0x26, 0x07, 0x28, 0x78, 0xd4};
     
     unsigned char * start = (unsigned char *)c;
 
-    //Get the int that starts at offset 7
+    // Get the int that starts at offset 7
     int i =  0;      //CHANGE THIS LINE
 
-    //Print out int in decimal
+    // Print out int in decimal
+	// Should print "i :  224224224"
     printf("i : %d\n", i);
 
-    //Get the short that starts at offset 15
+    // Get the short that starts at offset 15
     short s = 0;    //CHANGE THIS LINE
-    //Print out short in decimal
+
+    // Print out short in decimal
+	// Should print "s : 1830"
     printf("s : %hd\n", s);
 
     return 0;
